e20051dd49cd7db932c8fc103caa0c1248bbd154
ToNgocHai1543 <161561980+ToNgocHai1543@users.noreply.github.com>
2024-05-04 01:17:51 +0700

    Update Game_map.cpp

--- Game_map.cpp
+++ Game_map.cpp
@@ -45,7 +45,7 @@ void GameMap::LoadMap(char* name)
 
 void GameMap::LoadTiles(SDL_Renderer* screen)
 {
-    char file_img[10];
+    char file_img[30];
     FILE* fp = NULL;
 
     for (int i = 0; i < MAX_TILES; i++)
@@ -55,7 +55,6 @@ void GameMap::LoadTiles(SDL_Renderer* screen)
         fopen_s(&fp, file_img, "rb");
         if (fp == NULL)
         {
-
             continue;
         }
         fclose(fp);
@@ -75,11 +74,11 @@ void GameMap::DrawMap(SDL_Renderer* screen)
     int map_x = 0;
     int map_y = 0;
 
-    map_x = game_map_.start_x_/TILE_SIZE;
-    x1 = (game_map_.start_x_%TILE_SIZE)* - 1;
+    map_x = (game_map_.start_x_ % (MAX_MAP_X * TILE_SIZE + 1)) / TILE_SIZE;
+    x1 = (game_map_.start_x_ % TILE_SIZE)* (- 1);
     x2 = x1 + SCREEN_WIDTH + ( x1 == 0 ? 0 : TILE_SIZE);
 
-    map_y = game_map_.start_y_/TILE_SIZE;
+    map_y = (game_map_.start_y_ % (MAX_MAP_Y * TILE_SIZE + 1))/TILE_SIZE;
     y1 = (game_map_.start_y_ %TILE_SIZE)* - 1;
     y2 = y1 + SCREEN_HEIGHT + (y1 == 0 ? 0 : TILE_SIZE);
 
@@ -88,7 +87,7 @@ void GameMap::DrawMap(SDL_Renderer* screen)
         map_x = game_map_.start_x_/TILE_SIZE;
          for (int j = x1; j < x2; j+= TILE_SIZE)
          {
-             int val = game_map_.tile[map_y][map_x];
+             int val = game_map_.tile[map_y ][map_x % MAX_MAP_X];
              if (val > 0)
              {
                  tile_mat[val].SetRect(j, i);
