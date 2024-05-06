06d13d13e21ccea30eb4996a93fa149dfc767db6
ToNgocHai1543 <161561980+ToNgocHai1543@users.noreply.github.com>
2024-05-06 23:03:08 +0700

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -3,7 +3,6 @@
 #include "CommonFunc.h"
 #include "BaseObject.h"
 #include "Menu_object.h"
-#include "Mouse.h"
 
 class Menu : BaseObject
 {
@@ -13,13 +12,30 @@ private:
     TextObject difficult_;
     TextObject how_;
 
+    TextObject _how_;
+
+    TextObject easy_;
+    TextObject normal_;
+    TextObject hard_;
+
+    TextObject play_agian;
+    TextObject quit;
+
+    TextObject resume_;
+    TextObject pau_quit;
     SDL_Rect frame_clip_[0];
-    //Mouse m_;
+
 public:
     Menu();
-    int ShowMenu(std:: string t_play, std::string t_exit,std::string t_difficult, std::string t_how, SDL_Renderer* screen );
+    int ShowMenu(std:: string t_play, std::string t_exit,std::string t_difficult, std::string t_how,
+                  SDL_Renderer* screen, Mix_Chunk* g_sound_character[10] );
     bool Load_Button(std:: string path, SDL_Renderer* screen);
     void Show_Button(SDL_Renderer* des,std:: string path, int button_x ,int button_y);
+    int Show_Option(SDL_Renderer* screen, std:: string t_play_again, std::string t_exit, Mix_Chunk* g_sound_character[10]);
+    int Show_Pause(SDL_Renderer* screen, std:: string t_resume, std::string t_quit, Mix_Chunk* g_sound_character[10]);
+    int Show_How (SDL_Renderer* screen, std:: string t_how_, Mix_Chunk* g_sound_character[10]);
+    int Show_Difficult (SDL_Renderer* screen, std:: string easy, std::string normal, std:: string hard, Mix_Chunk* g_sound_character[10]);
+    //bool mute;
 };
 
 #endif // Menu_H_
